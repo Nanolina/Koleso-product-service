9445a3d6e10d0ba709ab9348373c92c386db4f1e
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-03-07 12:16:19 +0400

    refactor: refactor the name of file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-export { changeToColorType } from './change-to-color-type';
+export { changeToColorType } from './change-to-color-type.utils';
 export { imageUploadOptions } from './file-upload.utils';
